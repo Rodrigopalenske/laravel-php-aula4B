5799a4005ac24a4278e9f68b67370dfd5a2b30d9
Rodrigo Palenske <rodrigopalenske@gmail.com>
2024-08-20 23:59:13 -0300

    rotas e controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//                               qual a controller     qual o método
+Route::get('/produto/{id}', [SiteController::class, 'index']);
